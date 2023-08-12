4d3da72d61f207f094cb3f0f649e0ed48bcce456
Sergey Savlevich <sa.savlevich@gmail.com>
2023-08-13 03:02:03 +0700

    commit

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,5 +1,6 @@
 document.body.onload = function() {
-   setTimeout(() => {
+   
+   setTimeout(function() {
       const preloader = document.querySelector('#preloader')
       if (!preloader.classList.contains('done')) preloader.classList.add('done')
    }, 1000);
